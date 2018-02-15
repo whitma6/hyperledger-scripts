e7c85c4690710eb6954c696829c2110ebc7c5059
whitma6 <mark.white@verizonwireless.com>
2018-02-15 16:09:15 +0000

    new files

---
+++ versions.sh
@@ -0,0 +1,14 @@
+# Print installation details for user
+echo ''
+echo 'Component versions installed are:'
+echo ''
+echo -n 'Node:           '
+node --version
+echo -n 'npm:            '
+npm --version
+echo -n 'Docker:         '
+docker --version
+echo -n 'Docker Compose: '
+docker-compose --version
+echo -n 'Python:         '
+python -V
