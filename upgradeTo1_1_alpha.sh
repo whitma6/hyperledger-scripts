a948adfd3897529f67fd3497fad113db99a53b0b
whitma6 <36510799+whitma6@users.noreply.github.com>
2018-02-19 13:31:58 -0500

    Update upgradeTo1_1_alpha.sh

--- upgradeTo1_1_alpha.sh
+++ upgradeTo1_1_alpha.sh
@@ -1,8 +1,8 @@
 cd ~
 
 #### Download/update to latest docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.18.0/docker-compose-$(uname -s)-$(uname -m)" > ./docker-compose
-sudo mv ./docker-compose /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.18.0/docker-compose-$(uname -s)-$(uname -m)" > ~/docker-compose
+sudo mv ~/docker-compose /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
  
 #### Confirm that the version shows as 1.18
